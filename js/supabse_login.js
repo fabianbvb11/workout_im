09103956564a9bd7f676633ab6203b2c9667125c
fabianbvb11 <fabian.keller@stud.fhgr.ch>
2023-10-09 10:49:16 +0200

    einheitliche schriften

--- js/supabse_login.js
+++ js/supabse_login.js
@@ -1,1 +1,14 @@
 import { supa } from "../js/supabase_config.js";
+
+
+console.log("00 JavaScript verbunden")
+
+// 1. **Alle Filme abrufen**: Hole alle Spalten aller Filme aus der Tabelle `movies`.
+
+async function selectAllusers() {
+    const { data, error } = await supa.from("userdata").select();
+  
+    return data;
+  }
+
+console.log('Alle User in der DB: ', selectAllusers());
