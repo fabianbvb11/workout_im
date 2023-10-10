d28941daeba37c305d4a57f89572cf767507b88e
fabianbvb11 <146704376+fabianbvb11@users.noreply.github.com>
2023-10-11 00:45:54 +0200

    Add files via upload

---
+++ js/2_password_logout.js
@@ -0,0 +1,26 @@
+import { supa } from "../js/supabase_config.js";
+
+// Logout logic
+async function logout() {
+    const { error } = await supa.auth.signOut();
+    if (error) {
+        console.error("Error during logout:", error);
+    } else {
+        console.log("User logged out successfully.");
+        window.location.href = "welcome.html";
+    }
+}
+
+document.getElementById('logoutButton').addEventListener('click', logout);
+
+supa.auth.onAuthStateChange((event, session) => {
+    if (event === "SIGNED_OUT") {
+        console.log("Benutzer ausgeloggt.");
+    }
+});
+
+const user = supa.auth.user();
+if (!user) {
+    console.log("Benutzer ausgeloggt.");
+}
+
