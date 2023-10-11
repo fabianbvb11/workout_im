2d62d79f6954b780a69ec35f6fcd69d6baa3b294
Joel Hutter <joel.hutter@stud.fhgr.ch>
2023-10-11 10:54:21 +0200

    Zeitauswahl ist jetzt klickbar

--- js/zeitauswahl.js
+++ js/zeitauswahl.js
@@ -1,5 +1,6 @@
 const timeOptions = document.querySelectorAll(".scrollable-time ul li");
 const selectedTimeElement = document.getElementById("selectedTime");
+const buttons = document.querySelectorAll('.button-list button');
 
 let selectedTime = 5; // Initial selected time in minutes
 
@@ -7,6 +8,22 @@ let selectedTime = 5; // Initial selected time in minutes
 function updateSelectedTime() {
     selectedTimeElement.textContent = `${selectedTime} Minutes`;
 }
+
+let activeButton = null;
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (activeButton) {
+            // Reset the color of the previously active button to black
+            activeButton.style.color = 'black';
+        }
+        // Set the current button as the active button
+        activeButton = button;
+        // Change the color of the clicked button to white
+        button.style.color = 'white';
+    });
+});
 
 // Add click event listeners to time options
 timeOptions.forEach((option) => {
