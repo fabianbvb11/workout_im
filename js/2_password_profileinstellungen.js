278efc5da1e7a644c62eb343d258add75ce3b581
fabianbvb11 <fabian.keller@stud.fhgr.ch>
2023-10-11 10:59:21 +0200

    profileinstellungen anzeige

--- js/2_password_profileinstellungen.js
+++ js/2_password_profileinstellungen.js
@@ -2,35 +2,36 @@ import { supa } from "../js/supabase_config.js";
 
 // Funktion zum Abrufen und Anzeigen des Benutzernamens
 async function displayUserName() {
-
     const user = supa.auth.user();
 
     if (user) {
         const { id } = user;
 
-    // Abrufen der Benutzerdaten aus der 'userdata'-Tabelle
+        // Abrufen der Benutzerdaten aus der 'userdata'-Tabelle
+        const { data, error } = await supa
+            .from('userdata')
+            .select('vorname', 'nachname')
+            .eq('id', id)
+            .single();
 
-    const { data, error } = await supa
-        .from('userdata')
-        .select('vorname', 'nachname')
-        .eq('id', id)
-        .single();
-
-    if (error) {
-        console.log('Fehler beim Abrufen der Benutzerdaten:', error);
-        document.getElementById('firstnameprofil').textContent = "unbekannt";
-        document.getElementById('lastnameprofil').textContent = "unbekannt";
-    } else {
-            // Den Vornamen und Nachnamen anzeigen
+        if (error) {
+            console.log('Fehler beim Abrufen der Benutzerdaten:', error);
+            document.getElementById('firstnameprofil').value = "unbekannt";
+            document.getElementById('lastnameprofil').value = "unbekannt";
+        } else {
+            
+            // Den Vornamen und Nachnamen in den Eingabefeldern anzeigen
             const vorname = `${data.vorname}`;
-            const nachname = `${data.nachname}`
-            document.getElementById('firstnameprofil').textContent = vorname;
-            document.getElementById('lastnameprofil').textContent = nachname;
+            const nachname = `${data.nachname}`;
+
+            console.log(vorname + nachname)
+
+
+            document.getElementById('firstnameprofil').value = vorname;
+            document.getElementById('lastnameprofil').value = nachname;
         }
     }
 }
 
 // Führen Sie die Funktion aus, um den Benutzernamen anzuzeigen
-
 displayUserName();
-
