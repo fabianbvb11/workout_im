dc1d2de23193e284eca226b8ecc76f7de2578525
fabianbvb11 <146704376+fabianbvb11@users.noreply.github.com>
2023-10-12 17:22:03 +0200

    profilbild anzeige

--- js/2_password_startscreen.js
+++ js/2_password_startscreen.js
@@ -3,14 +3,11 @@ import { supa } from "../js/supabase_config.js";
 // Funktion zum Abrufen und Anzeigen des Benutzernamens
 
 async function displayUserDisplayName() {
-
     const user = supa.auth.user();
 
     if (user) {
 
         const { id } = user;
-
-        // Abrufen der Benutzerdaten aus der 'userdata'-Tabelle
 
         const { data, error } = await supa
             .from('userdata')
@@ -18,15 +15,10 @@ async function displayUserDisplayName() {
             .eq('id', id)
             .single();
 
-
         if (error) {
-
             console.log('Fehler beim Abrufen der Benutzerdaten:', error);
             document.getElementById('userDisplayName').textContent = "unbekannt";
-
         } else {
-
-            // Den Vornamen und Nachnamen anzeigen
             const displayName = `${data.vorname}`;
             document.getElementById('userDisplayName').textContent = "Hallo " + displayName + "!";
 
@@ -36,9 +28,41 @@ async function displayUserDisplayName() {
 
 }
 
-
-
-// Führen Sie die Funktion aus, um den Benutzernamen anzuzeigen
-
 displayUserDisplayName();
 
+
+
+
+// Profilbild anzeigen
+async function getSignedUrl(filePath) {
+    const { data, error } = await supa.storage.from('profilbilder').createSignedUrl(filePath, 300);
+    if (error) {
+        console.error('Fehler beim Laden des Bildes:', error);
+        return null;
+    }
+    return data.signedURL;
+}
+
+async function fetchAndDisplayPhotos() {
+    const { data, error } = await supa.from("userimage").select("url");
+    if (error) {
+        console.error("Error fetching photos:", error);
+        return;
+    }
+    const photosContainer = document.getElementById('profilbild_anzeige');
+    for (const photo of data) {
+        const signedUrl = await getSignedUrl(photo.url);
+        if (signedUrl) {
+            const imgElement = document.createElement('img');
+            imgElement.src = signedUrl;
+            imgElement.alt = "Uploaded photo";
+            imgElement.width = 200;
+            photosContainer.appendChild(imgElement);
+            const captionElement = document.createElement('p');
+            captionElement.textContent = photo.caption;
+            photosContainer.appendChild(captionElement);
+        }
+    }
+}
+
+fetchAndDisplayPhotos();
---
+++ js/upload.js
@@ -0,0 +1,109 @@
+import { supa } from "../js/supabase_config.js";
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const uploadButton = document.getElementById('uploadButton');
+    uploadButton.addEventListener('click', uploadPhoto);
+});
+
+async function uploadPhoto() {
+    // Stellen Sie sicher, dass der Benutzer eingeloggt ist, bevor Sie fortfahren.
+    const user = supa.auth.user();
+    if (!user) {
+        console.error('Benutzer ist nicht eingeloggt.');
+        return;
+    }
+
+    const fileInput = document.getElementById('photoInput');
+
+    if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const filePath = `profilbilder/${user.id}/${file.name}`;
+
+        const { data: existingImageData, error: existingImageError } = await supa.from('userimage').select('url').eq('id', user.id);
+
+        if (existingImageError) {
+            console.error('Fehler beim Abfragen des vorhandenen Bilds:', existingImageError);
+            return;
+        }
+
+        if (existingImageData.length > 0) {
+            const existingImagePath = existingImageData[0].url;
+            const { error: deleteError } = await supa.storage.from('profilbilder').remove([existingImagePath]);
+
+            if (deleteError) {
+                console.error('Fehler beim Löschen des vorhandenen Bilds:', deleteError);
+                return;
+            }
+
+            const { error: deleteUserDataError } = await supa.from('userimage').delete().eq('id', user.id);
+
+            if (deleteUserDataError) {
+                console.error('Fehler beim Löschen des alten Datensatzes:', deleteUserDataError);
+                return;
+            }
+        }
+
+        const { error: uploadError } = await supa.storage.from('profilbilder').upload(filePath, file);
+
+        if (uploadError) {
+            console.error('Fehler beim Hochladen der Datei:', uploadError);
+            return;
+        }
+
+        // Einfügen der Daten in die Tabelle "userimage" mit Benutzer-ID
+        const { data, error } = await supa.from('userimage').insert([
+            {
+                url: filePath,
+                id: user.id // Hinzufügen der Benutzer-ID
+            }
+        ]);
+
+        if (error) {
+            console.error('Fehler beim Speichern in der Tabelle "userimage":', error);
+        } else {
+            console.log('Erfolgreich hochgeladen und gespeichert:', data);
+            alert('Profilbild erfolgreich hochgeladen!');
+        }
+    } else {
+        console.log('Keine Datei ausgewählt.');
+    }
+}
+
+
+
+
+// Profilbild anzeigen
+async function getSignedUrl(filePath) {
+    const { data, error } = await supa.storage.from('profilbilder').createSignedUrl(filePath, 300);
+    if (error) {
+        console.error('Fehler beim Laden des Bildes:', error);
+        return null;
+    }
+    return data.signedURL;
+}
+
+async function fetchAndDisplayPhotos() {
+    const { data, error } = await supa.from("userimage").select("url");
+    if (error) {
+        console.error("Error fetching photos:", error);
+        return;
+    }
+    const photosContainer = document.getElementById('profilbild_anzeige');
+    for (const photo of data) {
+        const signedUrl = await getSignedUrl(photo.url);
+        if (signedUrl) {
+            const imgElement = document.createElement('img');
+            imgElement.src = signedUrl;
+            imgElement.alt = "Uploaded photo";
+            imgElement.width = 200;
+            photosContainer.appendChild(imgElement);
+            const captionElement = document.createElement('p');
+            captionElement.textContent = photo.caption;
+            photosContainer.appendChild(captionElement);
+        }
+    }
+}
+
+fetchAndDisplayPhotos();
