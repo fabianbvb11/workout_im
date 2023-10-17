57a4cb78654f7cfcddaeef06d4ea22d7b793b354
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2023-10-17 09:52:41 +0200

    Vorgeschlagene Workouts Bugfixes
    -Thumbnails Added

--- js/vorgeschlageneWorkouts.js
+++ js/vorgeschlageneWorkouts.js
@@ -1,6 +1,12 @@
 import { supa } from "../js/supabase_config.js";
-
 console.log("Supabase initialisiert")
+
+// YT API Key
+const API_KEY = 'AIzaSyD62p9odxLl0hhLyUbCV76TVnndslpe_x8';
+
+
+
+
 
 //testen ob local storage funktioniert
 console.log(localStorage.getItem('activeDotID'));
@@ -20,49 +26,73 @@ let workoutEquipmenttisch = localStorage.getItem('tisch');
 let workoutMuskelgruppe = localStorage.getItem('activeDotID');
 let workoutDauer = localStorage.getItem('selectedTime');
 
+
 //abfrage von Supabase
+// ...
+
 async function selectAllWorkouts() {
-    
     const { data, error } = await supa
-    .from("workoutdata")
-    .select()
-    .lte('zeit', workoutDauer)
-    .eq('muskelgruppe', workoutMuskelgruppe)
-    .eq('bett', workoutEquipmentbett)
-    .eq('stuhl', workoutEquipmentstuhl)
-    .eq('hantel', workoutEquipmenthantel)
-    .eq('tisch', workoutEquipmenttisch)
-    
+        .from("workoutdata")
+        .select()
+        .lte('zeit', workoutDauer)
+        .eq('muskelgruppe', workoutMuskelgruppe)
+        .eq('bett', workoutEquipmentbett)
+        .eq('stuhl', workoutEquipmentstuhl)
+        .eq('hantel', workoutEquipmenthantel)
+        .eq('tisch', workoutEquipmenttisch)
+
     if (error) {
         console.log("error", error);
         return;
     }
     console.log("data", data);
 
+    const resultContainer = document.querySelector("#result-container");
+
     for (const workout of data) {
         console.log(workout.titel);
 
-        const resultContainer = document.querySelector("#result-container");
+        // Replace 'YOUR_YOUTUBE_URL' with the YouTube URL you want to extract a thumbnail from
+        const YOUTUBE_URL = `${workout.video}`;
+        // Parse the video ID from the YouTube URL
+        const videoId = new URL(YOUTUBE_URL).searchParams.get('v');
 
-        resultContainer.innerHTML += `
-        <a href="./videoplayer.html?id=${workout.id}">
-            <div class="Button1">
-                <img src="../img/Play_Icon.svg" alt="Workout">
-                <div class="WorkoutButton">
-                    <h6>${workout.titel}</h6>
-                </div>
-            </div>
-        </a>
-        `;
+        // Use the YouTube Data API to get video details, including thumbnails
+        const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
 
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.items && data.items.length > 0) {
+                    const thumbnailUrl = data.items[0].snippet.thumbnails.default.url;
+                    console.log('Thumbnail URL:', thumbnailUrl);
 
+                    const thumbnailImage = document.createElement('img'); // Create the thumbnail image element for each video
+                    thumbnailImage.src = thumbnailUrl;
+
+                    resultContainer.innerHTML += `
+                    <a href="./videoplayer.html?id=${workout.id}">
+                        <div class="Button1">
+                            <img class="thumbnail" src="${thumbnailImage.src}" alt="YouTube Thumbnail">
+                            <h6>${workout.titel}</h6>
+                        </div>
+                    </a>
+                    `;
+                } else {
+                    console.error('Unable to fetch thumbnail.');
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
+}
+
+selectAllWorkouts();
 
 
-    // return data;
-  }
 
 
-  selectAllWorkouts();
+
 
 
