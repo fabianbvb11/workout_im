cc1f8c03f81f3642a38647879026b82416cd8230
Joel Hutter <joel.hutter@stud.fhgr.ch>
2023-10-19 13:54:34 +0200

    Equipment nichtsbutton gebastel

--- js/equipment.js
+++ js/equipment.js
@@ -2,6 +2,55 @@
 const weiterButton  = document.querySelector('#equipmentAbfrage');
 weiterButton.addEventListener('click', abfrageEquipment);
 
+// Funktion zur Zurücksetzung der anderen Buttons
+function resetOtherButtons() {
+    const buttons = ["bettbutton", "stuhlbutton", "hantelbutton", "tischbutton"];
+    buttons.forEach((buttonId) => {
+        document.getElementById(buttonId).checked = false;
+    });
+}
+
+// Funktion zur Behandlung des "Nichtsbutton" Klicks
+function handleNichtsButtonClick() {
+    resetOtherButtons();
+}
+
+// Event Listener für den "Nichtsbutton"
+const nichtsButton = document.getElementById("nichtsbutton");
+nichtsButton.addEventListener('change', function() {
+    if (nichtsButton.checked) {
+        handleNichtsButtonClick();
+    }
+});
+
+// Funktion zur Zurücksetzung des "Nichtsbutton"
+function resetNichtsButton() {
+    document.getElementById("nichtsbutton").checked = false;
+}
+
+// Funktion zur Behandlung des Klicks auf die anderen Buttons
+function handleOtherButtonClick() {
+    resetNichtsButton();
+}
+
+// Event Listener für die anderen Buttons
+const otherButtons = document.querySelectorAll(".container input:not(#nichtsbutton)");
+otherButtons.forEach((button) => {
+    button.addEventListener('change', function () {
+        if (button.checked) {
+            handleOtherButtonClick();
+        }
+    });
+});
+
+// Event Listener für den "Nichtsbutton"
+const nichtsButtonUnchecked = document.getElementById("nichtsbutton");
+nichtsButton.addEventListener('change', function () {
+    if (nichtsButton.checked) {
+        resetNichtsButton();
+    }
+});
+
 
 async function abfrageEquipment() {
  
@@ -11,18 +60,22 @@ async function abfrageEquipment() {
     const workoutEquipmentstuhl = document.getElementById("stuhlbutton").checked;
     const workoutEquipmenthantel = document.getElementById("hantelbutton").checked;
     const workoutEquipmenttisch = document.getElementById("tischbutton").checked;
-    
+    const workoutEquipmentnichts = document.getElementById("nichtsbutton").checked;
+
+
     //Auswahl ins local storage schreiben
     localStorage.setItem('bett', workoutEquipmentbett);
     localStorage.setItem('stuhl', workoutEquipmentstuhl);
     localStorage.setItem('hantel', workoutEquipmenthantel);
     localStorage.setItem('tisch', workoutEquipmenttisch);
+    localStorage.setItem('nichts', workoutEquipmentnichts);
 
     //Auswahl aus local storage ins console log schreiben
     console.log(localStorage.getItem('bett'));
     console.log(localStorage.getItem('stuhl'));
     console.log(localStorage.getItem('hantel'));
     console.log(localStorage.getItem('tisch'));
+    console.log(localStorage.getItem('nichts'));
 
     window.location.href = "vorgeschlageneworkouts.html";
 }
--- js/zeitauswahl.js
+++ js/zeitauswahl.js
@@ -52,7 +52,7 @@ weiterButton.addEventListener("click", () => {
     } else {
         // No time selected, display an error message in "ausgabefeld"
         const ausgabefeld = document.getElementById("ausgabefeld");
-        ausgabefeld.innerHTML = "Please select a time before proceeding.";
+        ausgabefeld.innerHTML = "Du hast noch keine Zeit ausgewählt";
     }
 });
 
